bd04112af53b70f5ec58d74c4ce4e1505a7ed16c
Bryan Cervantes <bryanandkyle@gmail.com>
2020-10-06 19:19:52 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,15 +5,15 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  "SELECT MAX(gpa) FROM students"
+  "SELECT MIn(gpa) FROM students"
 end
 
 def average_student_gpa
-  "SELECT MAX(gpa) FROM students"
+  "SELECT AVG(gpa) FROM students"
 end
 
 def total_tardies_for_all_students
-  "SELECT MAX(gpa) FROM students"
+  "SELECT SUM(gpa) FROM students"
 end
 
 def average_gpa_for_9th_grade
